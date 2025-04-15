ab866de199ea9cac1f25b5a34511cddf50973242
unknown <vitapoperechna@gmail.com>
2025-04-15 14:25:49 +0300

    added script

---
+++ js/script.js
@@ -0,0 +1,9 @@
+const keys = document.querySelectorAll('.key')
+
+keys.forEach(key => {
+    key.addEventListener('click', ()=> playNote(key))
+})
+function playNote(key){
+    const noteAudio = document.getElementById(key.dataset.note)
+    noteAudio.play()
+}
